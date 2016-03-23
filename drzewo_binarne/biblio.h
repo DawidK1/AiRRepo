9f9598865de2247d55773e0a59510ced15f3645c
MarekPi <marek.piekarz@gmail.com>
2016-03-23 21:59:04 +0100

    drzewo binarne

---
+++ drzewo_binarne/biblio.h
@@ -0,0 +1,25 @@
+#ifndef BIBLIO
+#define BIBLIO
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+	struct node* ojciec;
+	struct node* lewy_syn;
+	struct node* prawy_syn;
+	
+	char nazwisko[30];
+	char imie[30];
+	int numer_telefonu;
+}node;
+
+void insert(node* root, node* new_node);
+void in_order(node* root);
+node* find_min(node* root);
+node* find_max(node* root);
+//node* find_key(node* root, char* key); // uznaje, ze kluczem jest nazwisko
+
+
+#endif
