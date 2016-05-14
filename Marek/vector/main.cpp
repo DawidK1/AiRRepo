df740d48cbace406907ccd78f9f3337250b47547
MarekPi <marek.piekarz@gmail.com>
2016-05-14 13:07:57 +0200

    ciag dalszy

--- Marek/vector/main.cpp
+++ Marek/vector/main.cpp
@@ -2,7 +2,14 @@
 
 int main()
 {
-	cout<<"hello"<<endl;
+	Vector a(2);
+	cin >> a;
+	Vector b(2);
+	cin >> b;
+	cout << "A: " << a << endl;
+	cout << "B: " << b << endl;
+	a=b;
+	cout << "A: " << a << endl;
 
 	return 0;
 }
