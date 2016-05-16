3ce7c16db7eb8ed2eefbc3bc995af4ad793cb851
Dawid Kasieczka <dawidk421@gmail.com>
2016-05-16 22:38:23 +0200

    dodano reszte operatorow dla wektorow, brakuje 2 albo 3

--- Dawid/Macierze/main.cpp
+++ Dawid/Macierze/main.cpp
@@ -8,10 +8,12 @@ int main()
 	int tab[] = {2,4,8}; 
  
 	Wektor m = Wektor(3, tab);
-	Wektor n = Wektor(3, tab);
+	Wektor n = Wektor(m);
 	
 	cout << m + n;
 	cout << m - n;
 	cout << m*n;
+	cout << m * 7;
+	cout << 112*m ;
 	}
   
--- Dawid/Macierze/wektor.h
+++ Dawid/Macierze/wektor.h
@@ -13,16 +13,24 @@ class Wektor
 
 	Wektor(int d, int* t);
 	Wektor(int d = 2);
+	Wektor(const Wektor& source);
 	~Wektor();
 
 
 	friend ostream& operator<< (ostream& out, const Wektor& v);	
 	friend istream& operator>> (istream& in, Wektor& v);	
-	
+	friend Wektor operator* (const int& m, const Wektor& s);
+	friend bool operator== (const Wektor& v1, const Wektor& v2);
+	friend bool operator!= (const Wektor& v1, const Wektor& v2);
+
 	Wektor& operator= (const Wektor& s);
 	Wektor operator+ (const Wektor& s) const;
 	Wektor operator- (const Wektor& s) const;
 	int operator* (const Wektor& s) const;
+	Wektor operator* (const int& m) const;
+	Wektor operator+= (const Wektor& s);
+	Wektor operator-= (const Wektor& s);
+	Wektor operator*= (const int& m);
 	double get(int i);
 
 
