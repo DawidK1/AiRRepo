4181520af85885dc838d46188af27c0fee6b7396
MarekPi <marek.piekarz@gmail.com>
2016-12-27 19:18:16 +0100

    program5 gotowy

--- Marek/problem5/program5.cpp
+++ Marek/problem5/program5.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector> //vector class-template
 #include <set>
+#include <map>
 
 #include <algorithm>
 #include <numeric>
 
 using namespace std;
 
+typedef multimap <int, string, less<int> > mp_type;
 int main()
 {
     cout << "ZADANIE A - VECTOR" << endl << endl;
@@ -29,7 +31,7 @@ int main()
     sort(v.begin(), v.end());
     reverse(v.begin(), v.end());
     cout << "Ilosc dziewiatek: " << count(v.begin(), v.end(), 9) <<endl;
-    cout << "Suma 4 pierwszych: " << accumulate(v.begin(), v.begin()+4, 0)<<endl;
+    cout << "Suma 4 pierwszych elementow: " << accumulate(v.begin(), v.begin()+4, 0)<<endl;
 
     // display info about v
     cout << "The size of v is: " << v.size() << "\nThe capacity of v is: " << v.capacity();
@@ -40,13 +42,13 @@ int main()
     {
     cout << *it << " ";
     }
-    cout << endl << endl;
+    cout << endl << "==================================" << endl << endl;
     //====================================================
 
     cout << "ZADANIE B SET" << endl << endl;
 
     set<int, less <int> > s;
-
+    
     s.insert(2);
     s.insert(3);
     s.insert(4);
@@ -62,13 +64,62 @@ int main()
     cout << "Ilosc dziewiatek: " << count(s.begin(), s.end(), 9) <<endl;
     cout << "Suma wszystkich elementow: " << accumulate(s.begin(), s.end(), 0)<<endl;
     
+    cout <<"\nThe content of s is: ";
     set<int, less <int> > ::iterator it_s;
     for (it_s = s.begin(); it_s != s.end(); it_s++)
     {
         cout << *it_s << " ";
     }
-    cout << endl << endl;
+    cout << endl << "==================================" << endl << endl;
     //====================================================
 
+    cout << "Zadanie C MULTIMAP" << endl << endl;
+
+    mp_type mp;
+    mp_type::iterator it_mp;
+
+    mp.insert(pair<int, string>(2, "dwa"));
+    mp.insert(pair<int, string>(3, "trzy"));
+    mp.insert(pair<int, string>(4, "cztery"));
+    mp.insert(pair<int, string>(7, "siedem"));
+    mp.insert(pair<int, string>(5, "piec"));
+
+    mp.insert(pair<int, string>(7, "siedem"));
+    mp.erase(mp.begin());
+    mp.insert(pair<int, string>(9, "dziewiec"));
+
+    pair <int, string> temp = *mp.begin();
+    it_mp = ++mp.begin();
+    mp.insert(temp);
+    mp.insert(*it_mp);
+
+    pair <mp_type::iterator, mp_type::iterator> three;
+    three = mp.equal_range(3);
+    cout << "Elementy o wartosci 3: " << endl;
+    mp_type::iterator ite;
+    for (ite = three.first; ite != three.second; ++ite)
+    {
+        cout << "[" << ite->first << " - " << ite->second << "]" << endl;
+    }
+
+    mp.erase(3);
+	cout << "\nElement o najmniejszej wartosci: "<< endl << "[" << (*min_element(mp.begin(), mp.end())).first << " - " << (*min_element(mp.begin(), mp.end())).second << "]" << endl;
+    cout << "\nIlosc dziewiatek: " << mp.count(9) << endl;
+    cout << "Suma 4 pierwszych elementow: ";
+    int sum = 0;
+    it_mp = mp.begin();
+    for (int i = 0; i<4; i++)
+    {
+        sum += (*it_mp).first;
+        it_mp++;
+    }
+    cout << sum << endl;
+    
+    //
+    cout << "\nThe content of mp is: " << endl;
+    for (it_mp = mp.begin(); it_mp != mp.end(); it_mp++)
+    {
+        cout << "[" << (*it_mp).first << " - " << (*it_mp).second << "]" << endl;
+    }
     return 0;
 }
